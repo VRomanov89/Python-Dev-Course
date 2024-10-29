18989e0f5ee6ed98fbe26d959b42293266b2fc5a
Vladimir Romanov <vladromweb@gmail.com>
2024-10-28 22:29:59 -0400

    Big CSV Parsing
    Big CSV Parsing

--- 25 CSV Files/main.py
+++ 25 CSV Files/main.py
@@ -6,21 +6,21 @@ with open ("weather_data.csv") as data_file:
     for row in data:
         if row[1] != "temp":
             temperatures.append(int(row[1]))
-    print(temperatures) """
+    print(temperatures)
 
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-""" print(type(data))
+print(type(data))
 print(data["temp"])
 
 data_dict = data.to_dict()
 print(data_dict)
-print(data["temp"].max()) """
+print(data["temp"].max())
 
 
-""" print(data[data.day == "Monday"])
-print(data[data.temp == data.temp.max()]) """
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
 
 #Dataframe from scratch?
 data_dict = {
@@ -28,4 +28,23 @@ data_dict = {
     "scores": [76, 54, 34]
 }
 data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
+data.to_csv("new_data.csv") """
+
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
