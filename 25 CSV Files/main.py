24cf89590a7a6e5220fbd210ab29f8fc91bbde3f
Vladimir Romanov <vladromweb@gmail.com>
2024-10-28 21:59:01 -0400

    New file using pandas
    New file using pandas

--- 25 CSV Files/main.py
+++ 25 CSV Files/main.py
@@ -11,4 +11,21 @@ with open ("weather_data.csv") as data_file:
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
+""" print(type(data))
 print(data["temp"])
+
+data_dict = data.to_dict()
+print(data_dict)
+print(data["temp"].max()) """
+
+
+""" print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()]) """
+
+#Dataframe from scratch?
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 54, 34]
+}
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
